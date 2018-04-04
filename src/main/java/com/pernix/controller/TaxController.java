532d3324fbaefd31c03e3337703f2a6a13753abf
Alfre97 <alfredo1997avila@gmail.com>
2018-04-04 14:58:02 -0600

    Printing the addTax request parameters.

--- src/main/java/com/pernix/controller/TaxController.java
+++ src/main/java/com/pernix/controller/TaxController.java
@@ -33,8 +33,8 @@ public class TaxController {
 		tax.setDocumentNumber(documentNumber);
 		tax.setDocumentType(documentType);
 		
-		tax.setTaxExonarated(null);
-		tax.setTaxTotal(null);
+		tax.setTaxExonarated("");
+		tax.setTaxTotal("");
 		tax.setService(null);
 
 		try {
