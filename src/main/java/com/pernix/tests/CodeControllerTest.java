0711d8373a3c04c920f8bf5cc65022e30a25d4d3
Alfre97 <alfredo1997avila@gmail.com>
2018-04-10 13:32:28 -0600

    JUnit dependency changed to version 4.12.

--- src/main/java/com/pernix/tests/CodeControllerTest.java
+++ src/main/java/com/pernix/tests/CodeControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
