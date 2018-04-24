50ba9f54428cb5d3bc6ade9ff703f67d51f58ab1
Alfre97 <alfredo1997avila@gmail.com>
2018-04-24 11:29:00 -0600

    All issues solved. Deployed in Heroku.

--- src/main/java/com/pernix/EInvoiceApp.java
+++ src/main/java/com/pernix/EInvoiceApp.java
@@ -17,11 +17,20 @@
 package com.pernix;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class EInvoiceApp{
 	
+	@RequestMapping("/")
+	String home() {
+		return "E - INVOICING APP STARTED";
+	}
     public static void main(String[] args) throws Exception {
     	 SpringApplication.run(EInvoiceApp.class, args);
     }
