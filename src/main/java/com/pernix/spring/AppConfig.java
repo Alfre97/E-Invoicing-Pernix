6392fed2d29672a41ed01ac556e09f85b095dd25
Alfre97 <alfredo1997avila@gmail.com>
2018-04-25 11:30:28 -0600

    Everything setup to heroku deploy | Datasource properties created | app and dev properties modified | Spring Data JPA config modified

--- src/main/java/com/pernix/spring/AppConfig.java
+++ src/main/java/com/pernix/spring/AppConfig.java
@@ -5,7 +5,8 @@ import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -15,27 +16,16 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = { "com.pernix.spring.repository" })
 public class AppConfig {
-
-	@Autowired
-	Environment environment;
-
+	
 	@Bean(destroyMethod = "close")
-	DataSource dataSource(Environment env) {
-		HikariConfig dataSourceConfig = new HikariConfig();
-		dataSourceConfig.setDriverClassName(env.getRequiredProperty("db.driver"));
-		dataSourceConfig.setJdbcUrl(env.getRequiredProperty("db.url"));
-		dataSourceConfig.setUsername(env.getRequiredProperty("db.username"));
-		dataSourceConfig.setPassword(env.getRequiredProperty("db.password"));
-
-		return new HikariDataSource(dataSourceConfig);
-	}
+    @ConfigurationProperties(prefix = "source.properties")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
 
 	@Bean
 	LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Environment env) {
@@ -45,25 +35,10 @@ public class AppConfig {
 		entityManagerFactoryBean.setPackagesToScan("com.pernix.spring.model");
 
 		Properties jpaProperties = new Properties();
-
-		// Configures the used database dialect. This allows Hibernate to create SQL
-		// that is optimized for the used database.
 		jpaProperties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
-
-		// Specifies the action that is invoked to the database when the Hibernate
-		// SessionFactory is created or closed.
 		jpaProperties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
-
-		// Configures the naming strategy that is used when Hibernate creates
-		// new database objects and schema elements
 		jpaProperties.put("hibernate.ejb.naming_strategy", env.getRequiredProperty("hibernate.ejb.naming_strategy"));
-
-		// If the value of this property is true, Hibernate writes all SQL
-		// statements to the console.
 		jpaProperties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
-
-		// If the value of this property is true, Hibernate will format the SQL
-		// that is written to the console.
 		jpaProperties.put("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
 
 		entityManagerFactoryBean.setJpaProperties(jpaProperties);
