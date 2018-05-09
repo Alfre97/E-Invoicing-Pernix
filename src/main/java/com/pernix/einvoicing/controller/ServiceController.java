a29f108792fcc1bd06ea7776de016313f711a8b7
Alfre97 <alfredo1997avila@gmail.com>
2018-05-09 08:58:43 -0600

    Task N3 ready for pull and tested.

--- src/main/java/com/pernix/einvoicing/controller/ServiceController.java
+++ src/main/java/com/pernix/einvoicing/controller/ServiceController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -113,6 +114,29 @@ public class ServiceController {
 			return new ResponseEntity<String>(jsonService, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<String>(HttpStatus.CONFLICT);
+		}
+	}
+	
+	@RequestMapping("/deleteService")
+	public ResponseEntity<Boolean> deleteService(@RequestParam Long serviceId) throws Exception {
+		Services service = new Services();
+		try {
+			service.setId(serviceId);
+			serviceService.deleteService(service);
+			return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Boolean>(false, HttpStatus.CONFLICT);
+		}
+	}
+	
+	@RequestMapping("/modifyService")
+	public ResponseEntity<Boolean> modifyService(@RequestBody Services service) throws Exception {
+		Boolean result = false;
+		try {
+			result = serviceService.updateService(service);
+			return new ResponseEntity<Boolean>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Boolean>(result, HttpStatus.CONFLICT);
 		}
 	}
 }
--- src/main/java/com/pernix/einvoicing/controller/UserController.java
+++ src/main/java/com/pernix/einvoicing/controller/UserController.java
@@ -3,6 +3,7 @@ package com.pernix.einvoicing.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -83,6 +84,29 @@ public class UserController {
 			return new ResponseEntity<String>(jsonService, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<String>(HttpStatus.CONFLICT);
+		}
+	}
+	
+	@RequestMapping("/deleteUser")
+	public ResponseEntity<Boolean> deleteUser(@RequestParam Long userId) throws Exception {
+		UserEmitterReceiver user = new UserEmitterReceiver();
+		try {
+			user.setId(userId);
+			userService.deleteUser(user);
+			return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Boolean>(false, HttpStatus.CONFLICT);
+		}
+	}
+	
+	@RequestMapping("/modifyUser")
+	public ResponseEntity<Boolean> modifyUser(@RequestBody UserEmitterReceiver user) throws Exception {
+		Boolean result = false;
+		try {
+			result = userService.updateUser(user);
+			return new ResponseEntity<Boolean>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Boolean>(result, HttpStatus.CONFLICT);
 		}
 	}
 }
